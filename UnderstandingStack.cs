f65e47bcb84afb2247712367288131ee01913fd2
Trisshna <82799206+Trisshna@users.noreply.github.com>
2021-05-04 21:10:29 +0530

    Add files via upload

---
+++ UnderstandingStack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionProject
+{
+    class UnderstandingStack
+    {
+        Stack<string> mystack;
+        public UnderstandingStack()
+        {
+            mystack = new Stack<string>();
+        }
+        void AddItemsToStack()
+        {
+            //mystack.Push("Red");
+            //mystack.Push("Green");
+            //mystack.Push("Black");
+            mystack.Push("Red");
+            mystack.Push("Green");
+            mystack.Push("Black");
+
+        }
+        void PrintStack()
+        {
+            //foreach (var item in mystack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            while (mystack.Count > 0)
+            {
+                Console.WriteLine(mystack.Pop());
+            }
+            Console.WriteLine(mystack.Count);
+
+        }
+        static void Main(string[] a)
+        {
+            //    UnderstandingStack understandingStack = new UnderstandingStack();
+            //    understandingStack.AddItemsToStack();
+            //    understandingStack.PrintStack();
+            //    Console.ReadKey();
+        }
+    }
+}
